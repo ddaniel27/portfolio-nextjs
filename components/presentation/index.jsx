052570c9d37cad6ffa475d77c4043ce72b34cfb3
Daniel Dorado <danieldorado27@gmail.com>
2022-08-14 23:08:41 -0500

    [UPDATE] landing updated and repo goes remote

--- components/presentation/index.jsx
+++ components/presentation/index.jsx
@@ -1,9 +1,24 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import SpanAnimated from '../styledSpan'
 import style from './presentation.module.css'
 export default function Presentation(){
     return <div className={style.container}>
-        <h1>Daniel Dorado</h1>
+      <h1>
+	<SpanAnimated>D</SpanAnimated>
+	<SpanAnimated>a</SpanAnimated>
+	<SpanAnimated>n</SpanAnimated>
+	<SpanAnimated>i</SpanAnimated>
+	<SpanAnimated>e</SpanAnimated>
+	<SpanAnimated>l</SpanAnimated>
+	<span>&nbsp;</span>
+	<SpanAnimated>D</SpanAnimated>
+	<SpanAnimated>o</SpanAnimated>
+	<SpanAnimated>r</SpanAnimated>
+	<SpanAnimated>a</SpanAnimated>
+	<SpanAnimated>d</SpanAnimated>
+	<SpanAnimated>o</SpanAnimated>
+      </h1>
         <div className={style.textContainer}>
             <p className={style.fullerrorText}><span>Full</span></p>
             <p className={style.backerrorText}><span>Back</span></p>
